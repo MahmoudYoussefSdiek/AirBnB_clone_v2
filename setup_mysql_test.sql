1b1912b2b4bd57dd11c5c2bd61364f7651d938b3
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-09-20 08:18:30 -0700

    Create setup_mysql_test.sql for Test Database

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- Create MySQL Test Database
+
+CREATE DATABASE IF NOT EXSIST hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES  ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost'  IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON `performance_schema`.*  TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+FLUSH PRIVILEGES;
