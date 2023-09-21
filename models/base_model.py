35002cf864ac9a90dc6f238f0180c1dd31ec5271
mahmoudyoussefsdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-09-20 19:47:13 -0700

    Update base_model.py file

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from models import storage
+import models
 
 Base = declarative_base()
 
@@ -21,7 +21,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -38,8 +37,8 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -53,4 +52,4 @@ class BaseModel:
 
     def delete(self):
         """Deletes the current instance from storage"""
-        storage.delete(self)
+        models.storage.delete(self)
