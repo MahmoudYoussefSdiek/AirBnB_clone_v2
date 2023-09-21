21ad0efd758ba1449ae2c9a1b880c50282a70bc7
mahmoudyoussefsdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-09-20 19:53:36 -0700

    Fix code in user.py file

--- models/user.py
+++ models/user.py
@@ -5,6 +5,7 @@ This module defines a User class.
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -14,7 +15,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    def __init__(self, *args, **kwargs):
-        """Instantiates a new User object"""
-        super().__init__(*args, **kwargs)
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
